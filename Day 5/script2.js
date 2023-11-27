2f2f41043c20141749e3fd098aa89a24c79ba11d
vishal Karale <vishalkaralevsk@gmail.com>
2023-11-27 09:42:35 +0530

    task done

---
+++ Day 5/script2.js
@@ -0,0 +1,47 @@
+const resumeData = {
+  name: "Vishal Karale",
+  title: "Software Engineer",
+  contact: {
+    email: "vishalkaralevsk@example.com",
+    phone: "+1234567890",
+    address: "123 Main St, Ramling road , shirur, pune ",
+  },
+  summary:
+    "Experienced software engineer with a passion for building robust and scalable applications.",
+  experience: [
+    {
+      position: "Developer",
+      company: "Tech Solutions Inc.",
+      startDate: "2018",
+      endDate: "Present",
+      responsibilities: [
+        "Lead a team of developers in designing and implementing software solutions.",
+        "Developed and maintained critical systems for clients in various industries.",
+        "Collaborated with cross-functional teams to deliver high-quality products.",
+      ],
+    },
+    {
+      position: "intern Developer",
+      company: "CodeCrafters LLC",
+      startDate: "2015",
+      endDate: "2018",
+      responsibilities: [
+        "Contributed to the development of front-end and back-end systems.",
+        "Participated in code reviews and provided technical guidance to junior developers.",
+      ],
+    },
+  ],
+  education: [
+    {
+      degree: "Bachelor of Science in Computer Science",
+      institution: "SPPU",
+      graduationYear: "2015",
+    },
+  ],
+  skills: ["JavaScript", "Node.js", "React", "Python", "SQL", "HTML", "CSS"],
+};
+
+
+// print that resume data -
+
+console.log(resumeData);

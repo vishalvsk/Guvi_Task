ac9edcfa37ecfd01058ae490e6036368d57d6de0
vishal Karale <vishalkaralevsk@gmail.com>
2023-12-05 12:26:42 +0530

    push done

--- Day 4/script.js
+++ Day 4/script.js
@@ -1,51 +1,58 @@
-// 1-  print odd numbers using anonimous function - 
-const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// // 1-  print odd numbers using anonimous function - 
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-const oddNumbers = numbers.filter((number) => number % 2 !== 0);
+// const oddNumbers = numbers.filter((number) => number % 2 !== 0);
 
-oddNumbers.forEach((number) => {
-  console.log(number);
-});
-
-
-// 2 - convert all the string to little caps using anonumious function -
-
-const stringArray = ["Hello", "WORLD", "JavaScript", "Example"];
-
-const lowercaseArray = stringArray.map((str) => str.toLowerCase());
-
-console.log(lowercaseArray);
+// oddNumbers.forEach((number) => {
+//   console.log(number);
+// });
 
 
-// 3 - calculate the sum of all elements present in an array -
+// // 2 - convert all the string to little caps using anonumious function -
 
-const number = [1, 2, 3, 4, 5];
+// const stringArray = ["Hello", "WORLD", "JavaScript", "Example"];
 
-const sum = number.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+// const lowercaseArray = stringArray.map((str) => str.toLowerCase());
 
-console.log(sum); 
+// console.log(lowercaseArray);
 
-//  4 - return all the prime numbers in an array -
 
-function isPrime(num) {
-  if (num <= 1) {
-    return false;
-  }
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) {
-      return false;
-    }
-  }
-  return true;
-}
+// // 3 - calculate the sum of all elements present in an array -
 
-function findPrimeNumbers(arr) {
-  return arr.filter(num => isPrime(num));
-}
+// const number = [1, 2, 3, 4, 5];
 
-const number2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// const sum = number.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
 
-const primeNumbers = findPrimeNumbers(number2);
+// console.log(sum); 
 
-console.log(primeNumbers); 
+// //  4 - return all the prime numbers in an array -
 
+// function isPrime(num) {
+//   if (num <= 1) {
+//     return false;
+//   }
+//   for (let i = 2; i <= Math.sqrt(num); i++) {
+//     if (num % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
+
+// function findPrimeNumbers(arr) {
+//   return arr.filter(num => isPrime(num));
+// }
+
+// const number2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+// const primeNumbers = findPrimeNumbers(number2);
+
+// console.log(primeNumbers); 
+
+let n = prompt("Enter the value of n");
+n = Number.parseInt(n);
+let i = 0;
+while (i < n) {
+  console.log(i);
+  i++;
+}  
